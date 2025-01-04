e0bfc248cc6f0dda0340dcd0fbda72e43243aafb
Zenatihamza <147726473+Zenatihamza@users.noreply.github.com>
2025-01-04 23:10:03 +0100

    Update hamza.c
    calculer frequences

--- hamza.c
+++ hamza.c
@@ -1,1 +1,15 @@
-hamza
+void calculerFrequences(char mots[MAX_MOTS][MAX_LONGUEUR_MESSAGE], int frequences, Emaildonnees, int taille, int estSpam) {
+    for (int i = 0; i < MAX_MOTS; i++) {
+        frequences[i] = 0;
+    }
+
+    for (int i = 0; i < taille; i++) {
+        if (donnees[i].estSpam == estSpam) {
+            for (int j = 0; j < MAX_MOTS; j++) {
+                if (strstr(donnees[i].contenu, mots[j]) != NULL) {
+                    frequences[j]++;
+                }
+            }
+        }
+    }
+}
