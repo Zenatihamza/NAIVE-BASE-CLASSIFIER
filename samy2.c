d085fb3be74e222fa90dffaa440304576d20182e
Samy24s <148077089+Samy24s@users.noreply.github.com>
2025-01-04 23:17:04 +0100

    ss
    calculer probabilite

---
+++ samy2.c
@@ -0,0 +1,11 @@
+double calculerProbabilite(char mots[MAX_MOTS][MAX_LONGUEUR_MESSAGE], int *frequences, int totalClasse, char *message) {
+    double probabilite = 1.0;
+
+    for (int i = 0; i < MAX_MOTS; i++) {
+        if (strstr(message, mots[i]) != NULL) {
+            probabilite *= ((double)frequences[i] + 1) / (totalClasse + MAX_MOTS); // Lissage de Laplace
+        }
+    }
+    return probabilite;
+}
+
