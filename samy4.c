16c6bd03c317d705e7b01afa5de258a5bbae6b4f
Samy24s <148077089+Samy24s@users.noreply.github.com>
2025-01-04 23:38:16 +0100

    Create samy4.c
    reponse oui ou nn

---
+++ samy4.c
@@ -0,0 +1,9 @@
+
+    if (probSpam > probNonSpam) {
+        printf("Le message est classé comme SPAM.\n");
+    } else {
+        printf("Le message est classé comme NON-SPAM.\n");
+    }
+
+    return 0;
+}
